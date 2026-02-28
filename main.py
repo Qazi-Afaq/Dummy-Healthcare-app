a58e1802938a5ad7b4a5c60a1f4851564affa399
Afaq Qazi <afaq2qazi@gmail.com>
2026-02-28 02:14:46 +0000

    refactored registration form to use FlaskForm and added login route has been added

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask , render_template , request , url_for
 from models import (db , User , Role)
 from sqlalchemy import (select)
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, SelectField
-
+from wtforms import BooleanField, StringField, PasswordField, validators, SelectField
+from flask_wtf import FlaskForm
 from flask_login import LoginManager
 
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "get_from_env_later"
 db.init_app(app)
 
 # login manager
@@ -58,9 +59,9 @@ with app.app_context():
     db.session.commit()
 
 # forms
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[validators.DataRequired(), validators.Length(min=4, max=25)])
+    email = StringField('Email Address', validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
@@ -73,7 +74,11 @@ class RegistrationForm(Form):
 # default page -> go to self registraton user
 @app.route("/" , methods=["GET" , "POST"])
 def register_user():
-    return render_template('register-user.html')
+    return render_template('register-user.html' , form=RegistrationForm())
+
+@app.route("/login" , methods=["GET" , "POST"])
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
